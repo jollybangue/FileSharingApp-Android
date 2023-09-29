9f34de1b31a956807722e97f5c83fed4a943ccf6
jollybangue <141067435+jollybangue@users.noreply.github.com>
2023-09-28 21:14:10 -0600

    FileSharingApp-Android - Initial commit
    A native Android app that enables users to upload, download, delete, and share files using Firebase features (Firebase Authentication, Firebase Cloud Storage and Firebase Realtime Database).

---
+++ app/src/main/java/com/rexmicrosystems/filesharingapp/MainActivity.kt
@@ -0,0 +1,19 @@
+//
+//  MainActivity.kt
+//  FileSharingApp
+//
+//  Created by Jolly BANGUE on 2023-09-28.
+//
+// Description: A native Android app that enables users to upload, download, delete, and share files using Firebase features (Firebase Authentication and Firebase Cloud Storage).
+
+package com.rexmicrosystems.filesharingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
