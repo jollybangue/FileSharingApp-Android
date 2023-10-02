50b744e2588f6adc15f3882a7de42868a96ab633
jollybangue <141067435+jollybangue@users.noreply.github.com>
2023-10-01 20:26:22 -0600

    Updated Login and Register activities
    Implemented the "Go to Register" and "Go to Login" intents.

--- app/src/main/java/com/rexmicrosystems/filesharingapp/LoginActivity.kt
+++ app/src/main/java/com/rexmicrosystems/filesharingapp/LoginActivity.kt
@@ -8,12 +8,23 @@
 
 package com.rexmicrosystems.filesharingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val buttonSignIn = findViewById<Button>(R.id.buttonSignIn)
+        val buttonGoToRegister = findViewById<Button>(R.id.buttonGoToRegister)
+
+        buttonGoToRegister.setOnClickListener {
+            Intent(this, RegisterActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
--- app/src/main/java/com/rexmicrosystems/filesharingapp/RegisterActivity.kt
+++ app/src/main/java/com/rexmicrosystems/filesharingapp/RegisterActivity.kt
@@ -1,11 +1,22 @@
 package com.rexmicrosystems.filesharingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val buttonCreateAccount = findViewById<Button>(R.id.buttonCreateAccount)
+        val buttonGoToLogin = findViewById<Button>(R.id.buttonGoToLogin)
+
+        buttonGoToLogin.setOnClickListener {
+            Intent(this, LoginActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
