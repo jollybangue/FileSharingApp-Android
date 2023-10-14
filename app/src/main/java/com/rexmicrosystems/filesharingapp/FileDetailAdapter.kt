1683be6d10e14758787d8904f824e9210e7ab5a5
jollybangue <141067435+jollybangue@users.noreply.github.com>
2023-10-13 21:27:42 -0600

    Defined and improved Alert Dialogs and UI actions

--- app/src/main/java/com/rexmicrosystems/filesharingapp/FileDetailAdapter.kt
+++ app/src/main/java/com/rexmicrosystems/filesharingapp/FileDetailAdapter.kt
@@ -6,8 +6,9 @@ import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
-class FileDetailAdapter(var fileDetailList: List<FileDetail>): RecyclerView.Adapter<FileDetailAdapter.FileDetailViewHolder>() {
+class FileDetailAdapter(private var fileDetailList: List<FileDetail>): RecyclerView.Adapter<FileDetailAdapter.FileDetailViewHolder>() {
 
     class FileDetailViewHolder(fileDetailView: View): RecyclerView.ViewHolder(fileDetailView) {
         val textViewFileName: TextView = fileDetailView.findViewById(R.id.textViewFileName)
@@ -23,16 +24,39 @@ class FileDetailAdapter(var fileDetailList: List<FileDetail>): RecyclerView.Adap
     }
 
     override fun onBindViewHolder(holder: FileDetailViewHolder, position: Int) {
-        holder.textViewFileName.text = fileDetailList[position].name
+
+        // Alert Dialog buttons: Neutral: "FILE DETAILS", Positive: "SHARE", Negative: "CANCEL"
+        val fileAction = arrayOf("Open in Image View", "Open in Web View", "Open with System", "Download in the Default App Folder", "Download in Specified Location")
+
+        var fileSelectedName = fileDetailList[position].name
+
+        holder.textViewFileName.text = fileSelectedName
 
         holder.itemView.setOnClickListener {
-            //Toast.makeText(this, "Recycler View touched", Toast.LENGTH_SHORT).show()
-            Toast.makeText(it.context, "Item \"${holder.textViewFileName.text}\" selected", Toast.LENGTH_SHORT).show()
+
+            MaterialAlertDialogBuilder(it.context)
+                .setPositiveButton("SHARE") {_, _ ->
+                    Toast.makeText(it.context, "SHARE selected", Toast.LENGTH_SHORT).show()
+                }
+                .setNegativeButton("CANCEL") {_, _ ->
+                    Toast.makeText(it.context, "CANCEL selected", Toast.LENGTH_SHORT).show()
+                }
+                .setNeutralButton("FILE DETAILS") {_, _ ->
+                    Toast.makeText(it.context, "FILE DETAILS selected", Toast.LENGTH_SHORT).show()
+                }
+                .setItems(fileAction) { dialog, i ->
+                    Toast.makeText(it.context, "You selected \"${fileAction[i]}\" action", Toast.LENGTH_SHORT).show()
+
+                    println("Content of dialog local variable: $dialog")
+                    println("Content of i local variable: $i")
+                }
+                .setTitle(fileSelectedName)
+                .show()
+
             //notifyItemChanged(position)
-            println("Item ${holder.textViewFileName.text} selected")
 
             //    In "file_item.xml", android:foreground="?selectableItemBackground" allows to highlight the selected item.
-            //    Do I need  android:clickable="true" and android:focusable="true" ??? It seems NO.
+            //    Do I need  android:clickable="true" and android:focusable="true" ??? It seems NO!!!.
         }
     }
 
--- app/src/main/java/com/rexmicrosystems/filesharingapp/HomeActivity.kt
+++ app/src/main/java/com/rexmicrosystems/filesharingapp/HomeActivity.kt
@@ -12,13 +12,16 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.firebase.auth.FirebaseAuth
 
+
+// TODO: Improve alert dialog management
+
 class HomeActivity : AppCompatActivity() {
 
     companion object {
-        // We cannot create static variables in Kotlin. Instead, we should use a companion object
+        // We cannot create static variables in Kotlin. Instead, we should use a companion object.
         // All the fields of a companion object are static and therefore accessible from everywhere in the app.
         lateinit var appAuth: FirebaseAuth
-        var isNewUser: Boolean = false
+        var isNewUser: Boolean = false // isNewUser is set to true when a new user account is successfully created in RegisterActivity.
         var userEmail: String? = null // userEmail = appAuth.currentUser?.email when the user account is successfully created (see RegisterActivity).
         // As a rule, I decided that "userEmail" and all the companion fields (except "isNewUser") will be assigned a value ONLY from LoginActivity and RegisterActivity, and so NEVER from HomeActivity.
     }
@@ -37,7 +40,7 @@ class HomeActivity : AppCompatActivity() {
             MaterialAlertDialogBuilder(this)
                 // Note: With AlertDialog.Builder(this), while showing the dialog is less animated than with android.app.AlertDialog.Builder(this).
                 .setTitle("Account successfully created")
-                .setMessage("Welcome $userEmail. We hope you will enjoy this File Sharing app.")
+                .setMessage("Welcome $userEmail.\nWe hope you will enjoy this File Sharing App.")
                 .show()
             isNewUser = false // Once created the user is no longer new...
         }
@@ -49,6 +52,7 @@ class HomeActivity : AppCompatActivity() {
 
         textViewCurrentUser.text = userEmail // Setting the text of the Home label with the email of the currently logged user.
 
+        // Data source...
         var fileList = mutableListOf(
             FileDetail("id001", "Jolly.rex"),
             FileDetail("id002", "BBB.pdf"),
@@ -57,56 +61,44 @@ class HomeActivity : AppCompatActivity() {
             FileDetail("id007", "Linkin Park - What I've Done.mp3")
         )
 
-
-        var myAlertDialog = MaterialAlertDialogBuilder(this)
-
-
-        val actionItem = arrayOf("Item 1", "Item 2", "Item 3", "Item 4", "Item 5")
+        val uploadActionItem = arrayOf("Upload from Gallery", "Upload from Another Location")
 
         recyclerViewFileList.adapter = FileDetailAdapter(fileList)
         recyclerViewFileList.layoutManager = LinearLayoutManager(this)
         recyclerViewFileList.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL)) // Adding a divider to separate the items shown in the Recycler view.
-//        recyclerViewFileList.setOnClickListener {
-//            Toast.makeText(this, "Recycler View touched", Toast.LENGTH_SHORT).show()
-//        }
 
         buttonUpload.setOnClickListener {
             // TODO: Upload feature to be implemented...
-            myAlertDialog.setTitle("Upload file")
-            //myAlertDialog.setMessage("Hello Jolly. This is your first alert message!")
-            myAlertDialog.setPositiveButton("Positive 1") {_, _ ->
-            }
+            MaterialAlertDialogBuilder(this)
+                .setTitle("Upload a File")
+                .setPositiveButton("CANCEL") {_, _ ->
+                    Toast.makeText(this, "Upload cancelled", Toast.LENGTH_SHORT).show()
+                }
 
-            //myAlertDialog.create() // It seems that there is no need to invoke create() when show() will be invoked right after.
+                .setItems(uploadActionItem) { dialog, i ->
+                    Toast.makeText(this, "You selected ${uploadActionItem[i]}", Toast.LENGTH_SHORT).show()
 
-
-            myAlertDialog.setItems(actionItem) { dialog, i ->
-                Toast.makeText(this, "You selected ${actionItem[i]} and i = $i", Toast.LENGTH_LONG).show()
-                println("Content of dialog local variable: $dialog")
-                println("Content of i local variable: $i")
-            }
-
-
-
-            myAlertDialog.show()
-
+                    println("Content of dialog local variable: $dialog")
+                    println("Content of i local variable: $i")
+                }
+                .show() // It seems that there is no need to invoke create() when show() will be invoked right after.
         }
 
         buttonSignOut.setOnClickListener {
             // TODO: Add a confirmation dialog
-            myAlertDialog.setTitle("Confirmation")
+            MaterialAlertDialogBuilder(this)
+                .setTitle("Confirmation")
                 .setMessage("Do you want to sign out?")
                 .setNegativeButton("Cancel") { _, _ ->
-
+                    Toast.makeText(this, "Cancelled sign-out", Toast.LENGTH_SHORT).show()
                 }
                 .setPositiveButton("Sign Out") { _, _ ->
                     appAuth.signOut() // Add try...catch
                     Intent(this, LoginActivity::class.java).also {
-                        startActivity(it)
-                        finish()
-                        Toast.makeText(this, "User logged out successfully", Toast.LENGTH_SHORT).show()
+                        startActivity(it) // Start LoginActivity
+                        finish() // Destroys the Home activity. In that way, when the back button will be pushed after Signing out we won't be able to back to the Home screen.
+                        Toast.makeText(this, "User $userEmail logged out successfully", Toast.LENGTH_SHORT).show()
                     }
-                    // Here we need to destroy the Home activity. In that way, when the back button will be pushed after Signing out we won't be able to back to the Home screen.
 
                 }
                 .show()
