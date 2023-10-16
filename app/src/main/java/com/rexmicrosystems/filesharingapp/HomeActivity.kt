7fbebb7b935baf2d6ea16d2addd9c307b598838a
jollybangue <141067435+jollybangue@users.noreply.github.com>
2023-10-16 12:12:09 -0600

    Worked on the implementation of cloud storage and realtime database features

--- app/src/main/java/com/rexmicrosystems/filesharingapp/HomeActivity.kt
+++ app/src/main/java/com/rexmicrosystems/filesharingapp/HomeActivity.kt
@@ -9,12 +9,13 @@ import android.widget.Toast
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.gms.auth.api.signin.internal.Storage
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
-
+import com.google.firebase.storage.ktx.component1
+import com.google.firebase.storage.ktx.component2
 
 // TODO: Improve alert dialog management
 // TODO: Generate app documentation
@@ -32,20 +33,28 @@ class HomeActivity : AppCompatActivity() {
 
     private lateinit var textViewCurrentUser: TextView
     private lateinit var recyclerViewFileList: RecyclerView
-    private lateinit var buttonSignOut: Button
     private lateinit var buttonUpload: Button
+    private lateinit var buttonSignOut: Button
 
-    private val myStorageRef = Firebase.storage.reference // Firebase Cloud Storage reference of this app. iOS Swift: private let myStorageRef = Storage.storage().reference()
-    private val fileStorageRoot = "FileSharingApp" // Root of the app data in the Firebase Cloud Storage
+
+    private val myStorageRef = Firebase.storage.reference // Pointing to the Firebase Cloud Storage root folder. iOS Swift: private let myStorageRef = Storage.storage().reference()
+    private val fileStorageRoot = "FileSharingApp" // Root folder of the app data in the Firebase Cloud Storage.
+
+    private val realtimeDbRef = Firebase.database.reference // Pointing to the Firebase Realtime Database root node (It is the Realtime database reference). iOS Swift: private let realtimeDbRef = Database.database().reference()
+    private val realtimeDbRoot = "FileSharingApp" // Root folder of the app data in the Realtime database.
+
+    //private lateinit var realtimeFileList: MutableList<FileDetail> // NOTE: Difference between array and mutable list: An array has a fixed size, whereas the size of a mutable list can increase or decrease dynamically.
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        // Checking if the app should show a welcome AlertDialog to the newly created user.
         if (isNewUser) {
-            // isNewUser becomes true ONLY when the new user account is successfully created (when the "Create Account" button is tapped).
+            // isNewUser becomes true ONLY when the new user account is successfully created (when the "Create Account" button is tapped, in the RegisterActivity).
             MaterialAlertDialogBuilder(this)
-                // Note: With AlertDialog.Builder(this), while showing the dialog is less animated than with android.app.AlertDialog.Builder(this).
+                // Note: With AlertDialog.Builder(this), the dialog is less animated than with android.app.AlertDialog.Builder(this). We use MaterialAlertDialogBuilder(this) for the new Material3 design.
                 .setTitle("Account successfully created.")
                 .setMessage("Welcome $userEmail.\nWe hope you will enjoy this File Sharing App!")
                 .show()
@@ -54,8 +63,9 @@ class HomeActivity : AppCompatActivity() {
 
         textViewCurrentUser = findViewById(R.id.textViewCurrentUser)
         recyclerViewFileList = findViewById(R.id.recyclerViewFileList)
-        buttonSignOut = findViewById(R.id.buttonSignOut)
         buttonUpload = findViewById(R.id.buttonUpload)
+        buttonSignOut = findViewById(R.id.buttonSignOut)
+
 
         textViewCurrentUser.text = userEmail // Setting the text of the Home label with the email of the currently logged user.
 
@@ -65,10 +75,12 @@ class HomeActivity : AppCompatActivity() {
             FileDetail("id002", "BBB.pdf"),
             FileDetail("id003", "Video of my soccer goal.mp4"),
             FileDetail("id004", "Early bird.jpeg"),
-            FileDetail("id007", "Linkin Park - What I've Done.mp3")
+            FileDetail("id007", "Linkin Park - What I've Done.mp3"),
+            FileDetail("00100", "Metal Gear Solid V - The Phantom Pain.ps4")
         )
 
-        val uploadActionItem = arrayOf("Upload from Gallery", "Upload from Another Location") // TODO: Check if it is better to just use buttons instead, NOT this list of 2 elements.
+        copyDataFromStorageToRealtimeDB() // Getting the list of files available in the Firebase Cloud Storage and storing them in the Realtime Database.
+        getFileNamesFromRealtimeDB() // Get and observe the file list stored in the Realtime Database.
 
         recyclerViewFileList.adapter = FileDetailAdapter(fileDetailList)
         recyclerViewFileList.layoutManager = LinearLayoutManager(this)
@@ -76,6 +88,8 @@ class HomeActivity : AppCompatActivity() {
 
         buttonUpload.setOnClickListener {
             // TODO: Upload feature to be implemented...
+            val uploadActionItem = arrayOf("Upload from Gallery", "Upload from Another Location") // TODO: Check if it is better to just use buttons instead, NOT this list of 2 elements.
+
             val uploadDialog = MaterialAlertDialogBuilder(this)
             uploadDialog
                 .setTitle("Upload a File")
@@ -117,10 +131,23 @@ class HomeActivity : AppCompatActivity() {
 
 
     /**
-     * This function get the list of files stored in the Firebase cloud storage and save it into
+     * This function gets the list of files stored in the Firebase cloud storage and save it into
      * the Realtime database using the setFileNamesInRealtimeDB() function.
      */
     fun copyDataFromStorageToRealtimeDB() {
+//        myStorageRef.child(fileStorageRoot).listAll()
+//            .addOnSuccessListener { (items, prefixes) ->
+//
+//                realtimeDbRef.child(realtimeDbRoot).removeValue() // Deletes all the current values in realtime database app folder to avoid duplication issues.
+//
+//                for (prefix in prefixes) { // List of folder storage references.
+//                    // TODO: Implement folder management
+//                }
+//
+//                for (item in items) { // List of file storage references.
+//                    realtimeDbRef.child(realtimeDbRoot).
+//                }
+//            }
 
 
     }
@@ -142,7 +169,11 @@ class HomeActivity : AppCompatActivity() {
      * the Firebase cloud storage.
      */
     fun getFileNamesFromRealtimeDB() {
+        //lateinit var fileList: MutableList<FileDetail>
 
+
+
+        //return fileList
     }
 }
 
