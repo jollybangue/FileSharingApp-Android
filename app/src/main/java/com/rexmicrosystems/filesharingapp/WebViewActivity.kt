312483a9aa95f4e4ef4f2a811f616b92bec36efa
jollybangue <141067435+jollybangue@users.noreply.github.com>
2023-11-06 21:10:16 -0700

    Update WebViewActivity.kt
    Open PDF in Web View

--- app/src/main/java/com/rexmicrosystems/filesharingapp/WebViewActivity.kt
+++ app/src/main/java/com/rexmicrosystems/filesharingapp/WebViewActivity.kt
@@ -1,8 +1,10 @@
 package com.rexmicrosystems.filesharingapp
 
-import android.content.Intent
+import android.graphics.pdf.PdfRenderer
+import android.media.MediaParser.SeekableInputReader
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.ParcelFileDescriptor
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
@@ -14,13 +16,17 @@ class WebViewActivity : AppCompatActivity() {
 
         val webView = findViewById<WebView>(R.id.webView)
         val fileName = intent.getStringExtra("EXTRA_FILENAME")
-        val fileURLString = "https://firebasestorage.googleapis.com/v0/b/rexmicrosystems.appspot.com/o/FileSharingApp%2FFile%203.jpg?alt=media&token=d27439f5-916f-4698-ae72-21122369939e&_gl=1*u0bh98*_ga*MjUzMDU0Mzg2LjE2ODkzNjkwNDg.*_ga_CW55HF8NVT*MTY5OTA0MDU2Ny4xNDguMS4xNjk5MDQyMzI5LjYwLjAuMA.."
-
+        //val fileURLString = "https://firebasestorage.googleapis.com/v0/b/rexmicrosystems.appspot.com/o/FileSharingApp%2FResume%20-%20Jolly%20Bangue%20-%20M.Eng%20-%20Mobile%20App%20Developer.pdf?alt=media&token=bb99abbf-fff7-48a6-b3e3-4770d1d554aa&_gl=1*bztqki*_ga*MjAzOTI0ODQxNi4xNjkzMzY1NjQ0*_ga_CW55HF8NVT*MTY5OTMyODMxMC42NS4xLjE2OTkzMjgzMTguNTIuMC4w"
+        val fileURLString = "https://www.pdf995.com/samples/pdf.pdf"
         supportActionBar?.title = fileName
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-//        webView.webViewClient = WebViewClient()
+        webView.webViewClient = WebViewClient()
 //        webView.webChromeClient = WebChromeClient()
+        webView.settings.setSupportZoom(true)
+        //webView.settings.builtInZoomControls = true
+
+        //webView.settings.javaScriptEnabled = true
 
 //        webView.webViewClient = object: WebViewClient() {
 //            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
@@ -28,7 +34,11 @@ class WebViewActivity : AppCompatActivity() {
 //                return true
 //            }
 //        }
+        //webView.loadUrl("https://docs.google.com/gview?embedded=true&url=https://firebasestorage.googleapis.com/v0/b/rexmicrosystems.appspot.com/o/FileSharingApp%2FResume%20-%20Jolly%20Bangue%20-%20M.Eng%20-%20Mobile%20App%20Developer.pdf?alt=media&token=bb99abbf-fff7-48a6-b3e3-4770d1d554aa&_gl=1*bztqki*_ga*MjAzOTI0ODQxNi4xNjkzMzY1NjQ0*_ga_CW55HF8NVT*MTY5OTMyODMxMC42NS4xLjE2OTkzMjgzMTguNTIuMC4w")
+        webView.loadUrl("https://docs.google.com/gview?embedded=true&url=$fileURLString")
 
-        webView.loadUrl(fileURLString)
+//        webView.loadUrl("https://drive.google.com/viewerng/viewer?embedded=true&url=$fileURLString")
+//        webView.loadUrl(fileURLString)
+
     }
 }
